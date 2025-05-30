b088c807c985e49bf37e44265986c18be1feccce
Derin Karas <127653224+derinkaras@users.noreply.github.com>
2025-05-29 20:26:45 -0600

    Fixed page nav not starting at the top

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,19 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter, Routes, Route} from 'react-router-dom';
-import './index.css'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import './index.css';
+
 import Home from "./pages/Home.jsx";
 import Quote from "./pages/Quote.jsx";
 import About from "./pages/About.jsx";
 import NavBar from "./components/NavBar.jsx";
 import Footer from "./components/Footer.jsx";
-
+import ScrollToTop from "./components/ScrollToTop.jsx"; // 👈 import this
 
 createRoot(document.getElementById('root')).render(
     <StrictMode>
         <BrowserRouter>
+            <ScrollToTop /> {/* 👈 insert here */}
             <div className="flex flex-col min-h-screen">
                 <NavBar />
                 <div className="flex-grow">
